216804cddeee8a7ad24f63b7f2b1e47d7c4ce670
Michael Rhee <mjrhee@gmail.com>
2013-09-12 10:11:07 -0500

    Add edit page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,14 +13,21 @@ get '/user/:user_id' do
   erb :profile
 end
 
+get '/create' do
+  erb :create
+end
+
+get '/edit/:event_id' do
+  @event = Event.find(params[:event_id])
+  erb :edit
+end
+
 get '/logout' do
   session.clear
   redirect '/'
 end
 
-get '/create' do
-  erb :create
-end
+
 
 
 # POST ===================================
@@ -62,6 +69,22 @@ post '/create' do
   ends_at = Time.parse("#{date} #{params[:ends_at]}")
   user = User.find(session[:user_id])
   user.created_events.create(name: event_name,
+  	                 location: event_location,
+                     starts_at: starts_at,
+                     ends_at: ends_at
+  	                 )
+  redirect "/user/#{user.id}"
+end
+
+post '/edit/:event_id' do
+  user = User.find(session[:user_id])
+  event = Event.find(params[:event_id])
+  event_name = params[:event_name]
+  event_location = params[:event_location]
+  date = params[:date].to_date
+  starts_at = Time.parse("#{date} #{params[:starts_at]}")
+  ends_at = Time.parse("#{date} #{params[:ends_at]}")
+  event.update_attributes(name: event_name,
   	                 location: event_location,
                      starts_at: starts_at,
                      ends_at: ends_at
